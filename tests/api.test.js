35776e9aaf497d0ef25cc3f07d1b0187bf7b10ae
Sotiris Magionas <Smagionas@revolta.co>
2026-01-22 16:55:21 +0200

    Added test.

---
+++ tests/api.test.js
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+
+test("GitHub API returns the right response", async () => {
+  const data = await fetch(
+    "https://api.github.com/repos/mdn/content/stats/commit_activity",
+  ).then((res) => res.json());
+  // A year has 52 weeks
+  expect(data).toHaveLength(52);
+  expect(Object.keys(data[0])).toEqual(["total", "week", "days"]);
+  expect(Object.keys(data[data.length - 1])).toEqual(["total", "week", "days"]);
+});
